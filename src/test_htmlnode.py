bb25a0e3b9000f31026ba5bf4ce09b898e965fa5
ZukaZamamee <zamamee3@hotmail.com>
2025-08-02 14:33:08 -0700

    Added LeafNode and tests boot.dev static site generator Ch2L4

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -35,3 +35,34 @@ class TestHTMLNode(unittest.TestCase):
         expected = " href=\"https://www.google.com\" target=\"_blank\""
 
         self.assertEqual(result, expected)
+
+class TestLeafNode(unittest.TestCase):
+    def test_leaf_to_html_no_tag(self):
+        node = LeafNode(tag=None, value="Hello, world!")
+        self.assertEqual(node.to_html(), "Hello, world!")
+
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_leaf_to_html_tag_a_value_single_prop(self):
+        node = LeafNode(tag="a", value="Hello World!", props={"href": "https://www.google.com",})
+
+        result = node.to_html()
+        expected = "<a href=\"https://www.google.com\">Hello World!</a>"
+
+        self.assertEqual(result, expected)
+
+    def test_leaf_to_html_tag_a_value_multi_prop(self):
+        node = LeafNode(tag="a", value="Hello World!", props={"href": "https://www.google.com","target": "_blank",})
+
+        result = node.to_html()
+        expected = "<a href=\"https://www.google.com\" target=\"_blank\">Hello World!</a>"
+
+        self.assertEqual(result, expected)
+
+    def test_leaf_to_html_no_value(self):
+        node = LeafNode(None, None)
+
+        with self.assertRaises(ValueError):
+            node.to_html()
