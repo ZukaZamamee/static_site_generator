8df9c98a8d81dc0268bc294176ab56141c1ab388
ZukaZamamee <zamamee3@hotmail.com>
2025-08-05 17:23:02 -0700

    Configured generators to run off GitHub Pages. Boot.dev static site generator Ch5L5

--- src/generate_content.py
+++ src/generate_content.py
@@ -2,7 +2,7 @@ import os
 from block_markdown import markdown_to_html_node, extract_title
 from pathlib import Path
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(basepath, from_path, template_path, dest_path):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     try:
         with open(from_path, "r", encoding='utf-8') as f:
@@ -20,6 +20,8 @@ def generate_page(from_path, template_path, dest_path):
         titled_page = template.replace("{{ Title }}", title)
         #Replace content with content from html
         full_page = titled_page.replace("{{ Content }}", html)
+        href_replaced = full_page.replace("href=\"/", f"href=\"{basepath}")
+        src_replaced = href_replaced.replace("src=\"/", f"src=\"{basepath}")
 
         #prepare destination directory
         dest_dir = os.path.dirname(dest_path)
@@ -27,12 +29,12 @@ def generate_page(from_path, template_path, dest_path):
 
         #write content to directory
         with open(dest_path, "w", encoding="utf-8") as f:
-            f.write(full_page)
+            f.write(src_replaced)
 
     except Exception as e:
         raise Exception(f"Error: {e} while generating page at {from_path}") 
     
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(basepath, dir_path_content, template_path, dest_dir_path):
     try:
         for filename in os.listdir(dir_path_content):
             from_path = os.path.join(dir_path_content, filename)
@@ -40,8 +42,8 @@ def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
             #If content is a file, generate page
             if os.path.isfile(from_path):
                 dest_path = Path(dest_path).with_suffix(".html")
-                generate_page(from_path, template_path, dest_path)
+                generate_page(basepath, from_path, template_path, dest_path)
             else:
-                generate_pages_recursive(from_path, template_path, dest_path)
+                generate_pages_recursive(basepath, from_path, template_path, dest_path)
     except Exception as e:
         raise Exception(f"Error: {e} while generating pages")
--- src/main.py
+++ src/main.py
@@ -1,15 +1,23 @@
+import sys
 from file_handling import clean_directory, copy_files_recursive
 from generate_content import generate_pages_recursive
 
 dir_path_static = "./static"
-dir_path_public = "./public"
+dir_path_public = "./docs"
 dir_path_content = "./content"
 template_path = "./template.html"
 
 def main():
+    args = sys.argv
+    if len(args) == 2:
+        basepath = args[1]
+    else:
+        print("Basepath not provided, defaulting to /")
+        basepath = "/"
+    
     clean_directory(dir_path_public)
     copy_files_recursive(dir_path_static, dir_path_public)
-    generate_pages_recursive(dir_path_content, template_path, dir_path_public)
+    generate_pages_recursive(basepath, dir_path_content, template_path, dir_path_public)
 
 if __name__ == "__main__":
     main()
